423b65537c7f2c00d3ff2c3504f78534765f0085
ZakirAvrora <sultan.yelikbayev@nu.edu.kz>
2022-05-05 21:55:43 +0500

    Commit no1

--- task01-arrays.go
+++ task01-arrays.go
@@ -2,5 +2,11 @@ package homework
 
 func average(input [15]float32) (result float32) {
 	//Place your code here
+	var sum float32 = 0
+	for _, e := range input {
+		sum += e
+	}
+
+	result = sum / float32(len(input))
 	return
 }
--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,18 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
 	//Place your code here
+	keys := make([]int, 0, len(input))
+
+	for k := range input {
+		keys = append(keys, k)
+	}
+
+	sort.Ints(keys)
+	for _, e := range keys {
+		result = append(result, input[e])
+	}
 	return
 }
