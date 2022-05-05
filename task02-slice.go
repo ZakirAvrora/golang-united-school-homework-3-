51cae7fe134acae302a9b01a6394e2447b27c3ab
ZakirAvrora <sultan.yelikbayev@nu.edu.kz>
2022-05-05 22:04:25 +0500

    change no 2

--- task02-slice.go
+++ task02-slice.go
@@ -2,7 +2,7 @@ package homework
 
 func reverse(input []int64) (result []int64) {
 	//Place your code here
-	for i := len(input) - 1; i < 0; i-- {
+	for i := len(input) - 1; i >= 0; i-- {
 		result = append(result, input[i])
 	}
 
